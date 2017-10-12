e9f71f640d93f8a38af9ee9ed65135c9a7530993
Sergio Carvalho <sergio.carvalho@workday.com>
2017-10-12 22:49:01 +0100

    Change SQL statements to support CockroachDB

--- employees.sql
+++ employees.sql
@@ -22,11 +22,8 @@
 --  Any similarity to existing people is purely coincidental.
 -- 
 
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE IF NOT EXISTS employees;
-USE employees;
-
-SELECT 'CREATING DATABASE STRUCTURE' as 'INFO';
+DROP VIEW IF EXISTS dept_emp_latest_date,
+                    current_dept_emp;
 
 DROP TABLE IF EXISTS dept_emp,
                      dept_manager,
@@ -35,15 +32,12 @@ DROP TABLE IF EXISTS dept_emp,
                      employees, 
                      departments;
 
-/*!50503 set default_storage_engine = InnoDB */;
-/*!50503 select CONCAT('storage engine: ', @@default_storage_engine) as INFO */;
-
 CREATE TABLE employees (
     emp_no      INT             NOT NULL,
     birth_date  DATE            NOT NULL,
     first_name  VARCHAR(14)     NOT NULL,
     last_name   VARCHAR(16)     NOT NULL,
-    gender      ENUM ('M','F')  NOT NULL,    
+    gender      CHAR(1)         NOT NULL,
     hire_date   DATE            NOT NULL,
     PRIMARY KEY (emp_no)
 );
@@ -52,7 +46,7 @@ CREATE TABLE departments (
     dept_no     CHAR(4)         NOT NULL,
     dept_name   VARCHAR(40)     NOT NULL,
     PRIMARY KEY (dept_no),
-    UNIQUE  KEY (dept_name)
+    UNIQUE      (dept_name)
 );
 
 CREATE TABLE dept_manager (
@@ -60,8 +54,8 @@ CREATE TABLE dept_manager (
    dept_no      CHAR(4)         NOT NULL,
    from_date    DATE            NOT NULL,
    to_date      DATE            NOT NULL,
-   FOREIGN KEY (emp_no)  REFERENCES employees (emp_no)    ON DELETE CASCADE,
-   FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
+   FOREIGN KEY (emp_no)  REFERENCES employees (emp_no),
+   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
    PRIMARY KEY (emp_no,dept_no)
 ); 
 
@@ -70,8 +64,8 @@ CREATE TABLE dept_emp (
     dept_no     CHAR(4)         NOT NULL,
     from_date   DATE            NOT NULL,
     to_date     DATE            NOT NULL,
-    FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no)  ON DELETE CASCADE,
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
+    FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no,dept_no)
 );
 
@@ -80,7 +74,7 @@ CREATE TABLE titles (
     title       VARCHAR(50)     NOT NULL,
     from_date   DATE            NOT NULL,
     to_date     DATE,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     PRIMARY KEY (emp_no,title, from_date)
 ) 
 ; 
@@ -90,38 +84,18 @@ CREATE TABLE salaries (
     salary      INT             NOT NULL,
     from_date   DATE            NOT NULL,
     to_date     DATE            NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     PRIMARY KEY (emp_no, from_date)
-) 
-; 
+)
+;
 
-CREATE OR REPLACE VIEW dept_emp_latest_date AS
+CREATE VIEW dept_emp_latest_date AS
     SELECT emp_no, MAX(from_date) AS from_date, MAX(to_date) AS to_date
     FROM dept_emp
     GROUP BY emp_no;
 
-# shows only the current department for each employee
-CREATE OR REPLACE VIEW current_dept_emp AS
+CREATE VIEW current_dept_emp AS
     SELECT l.emp_no, dept_no, l.from_date, l.to_date
     FROM dept_emp d
         INNER JOIN dept_emp_latest_date l
         ON d.emp_no=l.emp_no AND d.from_date=l.from_date AND l.to_date = d.to_date;
-
-flush /*!50503 binary */ logs;
-
-SELECT 'LOADING departments' as 'INFO';
-source load_departments.dump ;
-SELECT 'LOADING employees' as 'INFO';
-source load_employees.dump ;
-SELECT 'LOADING dept_emp' as 'INFO';
-source load_dept_emp.dump ;
-SELECT 'LOADING dept_manager' as 'INFO';
-source load_dept_manager.dump ;
-SELECT 'LOADING titles' as 'INFO';
-source load_titles.dump ;
-SELECT 'LOADING salaries' as 'INFO';
-source load_salaries1.dump ;
-source load_salaries2.dump ;
-source load_salaries3.dump ;
-
-source show_elapsed.sql ;
